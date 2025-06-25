8127cc8cc3f51a3f7504638a514d37b78f8fc9f4
gitRaiku <rikunn.rikunn@gmail.com>
2025-06-25 16:01:01 +0300

    Add scroll handlers to prevent a dumb crash

--- src/config.h
+++ src/config.h
@@ -1,21 +1,21 @@
 #include <stdint.h>
 
-#define OVERRIDE_ZWLR_VERSION 0
+#define OVERRIDE_ZWLR_VERSION 3
 
 static const char statusPath[] = "/tmp/deburrstat"; // Location of the status pipe (through which you can set the status)
 
 
-static const double padding = 0.3; // Adds padding above and under the text
+static const double padding = 0.2; // Adds padding above and under the text
                                    // This value is them multiplied font height
 
-static const char *fonts[2] = { "JetBrainsMono:size=18", // Written using fontconfig selectors
-                                "Koruri:size=18" };      // Max 2 fonts, Leave NULL if you 
+static const char *fonts[2] = { "JetBrainsMono:size=16", // Written using fontconfig selectors
+                                "Koruri:size=16" };      // Max 2 fonts, Leave NULL if you 
                                                          // don't want a fallback font
 #define MANUAL_GAMMA 0 // Ignore
 static const double gammaCorrections[] = { 1.6, 1.6 }; // Gamma correction coefficient for each font
                                                        
-//static const wchar_t *tags[] = { L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", L"9" };
-static const wchar_t *tags[] = { L"壱", L"弐", L"参", L"四", L"五", L"六", L"七", L"八", L"九" };
+static const wchar_t *tags[] = { L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", L"9" };
+// static const wchar_t *tags[] = { L"壱", L"弐", L"参", L"四", L"五", L"六", L"七", L"八", L"九" };
 
 static const uint64_t col_gray   = 0xFF111111; // AARRGGBB
 static const uint64_t col_gray1  = 0xFF222222; // Truth be told idk
@@ -45,6 +45,6 @@ static const uint64_t colors[2][3]      = {
 };
 
 static const uint8_t log_level = 5;            /// Debug vars (0 -> print everything, 10 -> print errors)
-//static const char debugf[] = "/tmp/deburr-log"; /// If "" print to stderr
+// static const char debugf[] = "/tmp/deburr-log"; /// If "" print to stderr
 static const char debugf[] = ""; /// If "" print to stderr
 
